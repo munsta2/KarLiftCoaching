4a5a5cf7ad78ccc6246d9f7072e6c2fa6496defe
jesse <jessekarwaski@hotmail.com>
2023-10-18 17:46:17 -0400

    added more to the home page

--- karlift/src/Home.jsx
+++ karlift/src/Home.jsx
@@ -5,10 +5,30 @@ const Home = () => {
 
 
     <div class="flex flex-wrap h-screen">
-    <div class="w-1/2 h-1/2 bg-kar-white"></div>
-    <div class="w-1/2 h-1/2 bg-kar-light-grey"></div>
-    <div class="w-1/2 h-1/2 bg-kar-light-grey"></div>
-    <div class="w-1/2 h-1/2 bg-kar-white"></div>
+    <div class="w-1/2 h-1/2 bg-kar-white">
+
+      <h1>
+        Lifestyle Coaching
+      </h1>
+      
+      <p> 
+          The main goal of the serives I provide to my clients is to educate them 
+          on fitness related topics with the end goal being transforming there lifestyle to
+          be more active and healthy
+      </p>
+
+    </div>
+    <div class="w-1/2 h-1/2 bg-kar-light-grey">
+      <h1>Included in the service:</h1>
+    </div>
+    <div class="w-1/2 h-1/2 bg-kar-light-grey">
+      <h1>Pricing</h1>
+    </div>
+    <div class="flex items-center justify-center w-1/2 h-1/2 bg-kar-white">
+      <h1 className="text-center text-xl font-serif font-bold">if you wish to work with me to help you achive your goals head over to the
+          Application form page and submit your application and I'll reach out to you!
+      </h1>
+    </div>
   </div>
   
 
