6c13bf1eace778459d7f84ebdb4d26d8d76f1735
jesse <jessekarwaski@hotmail.com>
2023-10-19 13:33:44 -0400

    added some formatting

--- karlift/src/Footer.jsx
+++ karlift/src/Footer.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
     <footer className="fixed bottom-0 w-full bg-kar-blue text-center py-4">
-        <p>&copy; 2023 KarLift Coachng</p>
-  </footer>
+      <p>&copy; 2023 KarLift Coachng</p>
+    </footer>
   );
 };
 
--- karlift/src/NavBar.jsx
+++ karlift/src/NavBar.jsx
@@ -1,32 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';  // Import Link from react-router-dom
-import  Logo from './Logo.jsx'
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import Logo from "./Logo.jsx";
 const NavBar = () => {
   return (
     <div>
       <Logo />
-       <nav className="bg-kar-blue p-4">
- 
-
-            <ul className="flex items-center justify-center space-x-6">
-              <li className="mr-6">
-                <Link to="/" className="text-white hover:text-gray-300 font-bold text-lg">Home</Link>
-              </li>
-              <li className="mr-6">
-                <Link to="/application" className="text-white hover:text-gray-300 font-bold text-lg">Application Form</Link>
-              </li>
-              <li>
-                <Link to="/about" className="text-white hover:text-gray-300 font-bold text-lg">About Me</Link>
-              </li>
-            </ul>
-            </nav>
-
-
-
-      
+      <nav className="bg-kar-blue p-4">
+        <ul className="flex items-center justify-center space-x-6">
+          <li className="mr-6">
+            <Link
+              to="/"
+              className="text-white hover:text-gray-300 font-bold text-lg"
+            >
+              Home
+            </Link>
+          </li>
+          <li className="mr-6">
+            <Link
+              to="/application"
+              className="text-white hover:text-gray-300 font-bold text-lg"
+            >
+              Application Form
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className="text-white hover:text-gray-300 font-bold text-lg"
+            >
+              About Me
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
-
-   
   );
 };
 
